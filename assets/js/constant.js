ab5f82d1a1370cb59acb519051a80e88570ee3f4
obiwan <>
2024-05-24 10:31:56 +0800

    Updated details

---
+++ assets/js/constant.js
@@ -0,0 +1,136 @@
+let details = [
+    {
+        "name": "Memory",
+        "description": "A <b>Compose Multiplatform</b> project targeting Android and iOS. Also known as a matching game, is a classic card game where the objective is to find all matching pairs of cards.",
+        "image": "images/memory.webp",
+        "stack": "",
+        "links":[
+            {
+                "url": "https://github.com/ommagtibay/Memory-Compose-Multiplatform",
+                "icon": "icon solid brands fa-github",
+                "source": "Github"
+            }
+        ]
+    },
+    {
+        "name": "SF Retailer",
+        "description": "Android and iOS e-wallet applications utilized by distribution channels for the prepaid internet service of a leading ISP client.",
+        "image": "images/sfretailer.webp",
+        "stack": "Android SDK, Firebase Messaging, Analytics, Google Maps, iOS Maps, Kotlin, Swift, Storyboard, Room, MVVM",
+        "links":[
+            {
+                "url": "https://play.google.com/store/apps/details?id=com.superfiber.retailer",
+                "icon": "icon solid brands fa-google-play",
+                "source": "Google Play"
+            },
+            {
+                "url": "https://apps.apple.com/ph/app/sf-retailer/id6443470933",
+                "icon": "icon solid brands fa-apple",
+                "source": "App Store"
+            }
+        ]
+    },
+    {
+        "name": "Ngage",
+        "description": "A <i>digital marketing tool</i> that allows users to earn as many points as they can which they can use to redeem virtual rewards. Regarded as among the top local apps in the Google Play Store.",
+        "image": "images/ngage.webp",
+        "stack": "Android Studio, Java, DataBinding, ViewBinding, Firebase Cloud Messaging for push notification, Firebase Crashlytics for crash reporting solution, Firebase App Distribution for app testing, Facebook SDK (for login, share and shimmer), 3rd Party Libraries and APIs, Retrofit, Huawei HMS (Huawei Mobile Services), Glide, SQLite, Material Design, ConstraintLayout, LocationManager, SharedPreferences, ViewPager, TabLayout, RecyclerView, CardView, Navigation, NavigationDrawer and custom adapters and custom views (listviews, notifications, dialogs and other views).",
+        "links": []
+    },
+    {
+        "name": "Kweez",
+        "description": "A <i>trivia game</i> designed to test your wits on general topics of interest.",
+        "image": "images/kweez.webp",
+        "stack": "Android Studio, Java, Firebase Cloud Messaging for push notification, Firebase Crashlytics for crash reporting solution, Firebase App Distribution for app testing, Google Play Billing, Facebook SDK (for share and shimmer), 3rd Party Libraries and APIs, Retrofit (Socket protocol), Glide, Butterknife, SQLite, Material Design, ConstraintLayout, SharedPreferences, ViewPager, WebView, TabLayout, RecyclerView, CardView, Navigation and custom adapters and custom views (listviews, notifications, dialogs and other views).",
+        "links": []
+    },
+    {
+        "name": "Letsplay",
+        "description": "A <i>game</i> where players can obtain rewards by winning in live online battles. Letsplay is a live game app wherein you get to compete with other users and get a chance to win by joining the scheduled battle. Each users' highest score will be submitted as an entry during the event.",
+        "image": "images/letsplay.webp",
+        "stack": "Android Studio, Java, Firebase Cloud Messaging for push notification, Firebase Crashlytics for crash reporting solution, Firebase App Distribution for app testing, Facebook SDK (for share and shimmer), 3rd Party Libraries and APIs, Retrofit, Glide, Butterknife, SQLite, Material Design, ConstraintLayout, SharedPreferences, ViewPager, WebView, TabLayout, RecyclerView, CardView, Navigation and custom adapters and custom views (listviews, notifications, dialogs and other views).",
+        "links":[
+            {
+                "url": "https://play.google.com/store/apps/details?id=com.zedph.letsplay",
+                "icon": "icon solid brands fa-google-play",
+                "source": "Google Play"
+            }
+        ]
+    },
+    {
+        "name": "Pinoy",
+        "description": "A <i>pre-installed app</i> providing MyPhone and Zed services with custom prayer alarms and other items for download.",
+        "image": "images/pinoy.webp",
+        "stack": "Android Studio, Java, DataBinding, ViewBinding, Firebase Cloud Messaging for push notification, Firebase Analytics, Firebase App Distribution for app testing, 3rd Party Libraries and APIs, Retrofit, ZXing (Barcode scanning), Glide, SQLite, RSA Algorithm (for encryption and decryption), Material Design, ConstraintLayout, SharedPreferences, ViewPager, WebView, TabLayout, RecyclerView, CardView and custom adapters and custom views (listviews, notifications, dialogs and other views).",
+        "links": []
+    },
+    {
+        "name": "BeliGas (EDAR)",
+        "description": "Cooking Gas & Grocery Delivery App",
+        "image": "images/beligas.webp",
+        "stack": "Android Studio, Java, DataBinding, ViewBinding, Firebase Cloud Messaging for push notification, Firebase Crashlytics for crash reporting solution, Firebase App Distribution for app testing, Google (for Login, Maps and Location), Facebook SDK (for login and shimmer), RSA Algorithm (for encryption and decryption), 3rd Party Libraries and APIs, Retrofit, Glide, SQLite, Material Design, ConstraintLayout, SharedPreferences, WebView, RecyclerView, CardView, Navigation and custom adapters and custom views (listviews, notifications, dialogs and other views).",
+        "links":[
+            {
+                "url": "https://play.google.com/store/apps/details?id=my.beligas.delivery",
+                "icon": "icon solid brands fa-google-play",
+                "source": "Google Play"
+            }
+        ]
+    },
+    {
+        "name": "Flipbaq",
+        "description": "Social networking app for sharing photos and videos with friends.",
+        "image": "images/flipbaq.webp",
+        "stack": "",
+        "links": []
+    },
+    {
+        "name": "IberoPHTravel",
+        "description": "A platform that allows travel insurance policy holders to make claims and communicate with Ibero agents in real-time.",
+        "image": "images/iberophtravel.webp",
+        "stack": "",
+        "links": []
+    },
+    {
+        "name": "SuBuu",
+        "description": "Su Buu App prioritizes the customer experiences by providing one application with many desirable features to provide the best convenience for Su Buu user.",
+        "image": "images/subuu.webp",
+        "stack": "",
+        "links": []
+    },
+    {
+        "name": "vTuner",
+        "description": "A template app as client guide to use vTuner API",
+        "image": "images/vtuner.webp",
+        "stack": "",
+        "links": []
+    },
+    {
+        "name": "GraphCurrency",
+        "description": "App for converting selected currency to different currencies. With graph to show trends.",
+        "image": "images/graphcurrency.webp",
+        "stack": "",
+        "links": []
+    },
+    {
+        "name": "0hunB",
+        "description": "Interactive learning English app that feature phonics (beginner) to interview speaking level. Includes reading, listening materials, identifying words by selecting image from a list of choices.",
+        "image": "images/0hunb.webp",
+        "stack": "",
+        "links": []
+    },
+    {
+        "name": "VocaTree",
+        "description": "Interactive English vocabulary apps showing introductions to TOEIC English level. It includes a memory game where users have to spell the displayed Korean word in English while an audio of the English word is being played. It tracks user progress via graph by day, week and month.",
+        "image": "images/vocatree.webp",
+        "stack": "",
+        "links": []
+    },
+    {
+        "name": "",
+        "description": "",
+        "image": "images/",
+        "stack": "",
+        "links": []
+    }
+]
